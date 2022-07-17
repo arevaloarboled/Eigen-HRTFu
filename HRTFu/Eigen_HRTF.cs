837f61c4082eeca433ae839c4e4fa78243fb4e51
arevaloarboled <arevaloarboled@gmail.com>
2022-07-17 21:45:14 +0900

    V2

--- HRTFu/Eigen_HRTF.cs
+++ HRTFu/Eigen_HRTF.cs
@@ -6,34 +6,16 @@ namespace Eigen_HRTF_plugin {
     static class Eigen_HRTF
     {
         [DllImport("HRTFu_plugin", EntryPoint = "get_filters", CallingConvention = System.Runtime.InteropServices.CallingConvention.Cdecl)]
-        public static extern void get_filters( float d,  float e,  float a,  int pinna,  float[] filter_l,  float[] filter_r,  int[] delays,  int[] idxs);
-
-        //[DllImport("HRTFu_plugin", EntryPoint = "get_filters", CallingConvention = System.Runtime.InteropServices.CallingConvention.Cdecl)]
-        //public static extern unsafe void get_filters(float d, float e, float a, int pinna, float* filter_l, float* filter_r, int* delays, int* idxs);
-
-        //[DllImport("HRTFu_plugin", EntryPoint = "convolution", CallingConvention = System.Runtime.InteropServices.CallingConvention.Cdecl)]
-        //public static extern void convolution([Out] float[] signal, [In] float[] filter);
-
-        //[DllImport("HRTFu_plugin", EntryPoint = "convolution", CallingConvention = System.Runtime.InteropServices.CallingConvention.Cdecl)]
-        //public static extern unsafe void convolution( float* signal, float* filter);
+        public static extern void get_filters( float d,  float e,  float a,  int pinna,  float[] filter_l,  float[] filter_r,  int[] delays,  int[] idxs);        
 
         [DllImport("HRTFu_plugin", EntryPoint = "eigen_init", CallingConvention = System.Runtime.InteropServices.CallingConvention.Cdecl)]
-        public static extern void eigen_init();
+        public static extern void eigen_init([In] [MarshalAs(UnmanagedType.LPStr)] string path);        
+
+        [DllImport("HRTFu_plugin", EntryPoint = "full_DSP", CallingConvention = System.Runtime.InteropServices.CallingConvention.Cdecl)]
+        public unsafe static extern void full_DSP(float[] data, int data_size, float[] signal_buffer, float[] filter_l, float[] filter_r, float[] buffer_l, float[] buffer_r, int[] delays, float gain);
 
         [DllImport("HRTFu_plugin", EntryPoint = "DSP", CallingConvention = System.Runtime.InteropServices.CallingConvention.Cdecl)]
-        public unsafe static extern void DSP(float[] data, int data_size, float[] filter_l, float[] filter_r, float[] buffer_l, float[] buffer_r, int[] prev_delays, int[] delays);
+        public unsafe static extern void DSP(float[] data, int data_size, float[] filter_l, float[] filter_r, float[] filter_l2, float[] filter_r2, float[] buffer_l, float[] buffer_r, int[] prev_delays, int[] delays, int[] prev_ids, int[] ids, float gain);
 
-        //[DllImport("HRTFu_plugin", EntryPoint = "DSP", CallingConvention = System.Runtime.InteropServices.CallingConvention.Cdecl)]
-        //public unsafe static extern void DSP(float[] data, int data_size, float[] filter_l, float[] filter_r, float[] buffer_l, float[] buffer_r, int[] prev_delays, int[] delays, float[] extra_buffer);
-
-        //[DllImport("HRTFu_plugin", EntryPoint = "DSP", CallingConvention = System.Runtime.InteropServices.CallingConvention.Cdecl)]
-        //public static extern unsafe void DSP( float[] data, int data_size, float[] filter_l, float[] filter_r,  float[] buffer_l,  float[] buffer_r,  int[] prev_delays,  int[] delays);
-
-        //[DllImport("HRTFu_plugin", EntryPoint = "DSP", CallingConvention = System.Runtime.InteropServices.CallingConvention.Cdecl)]
-        //public static extern unsafe void DSP(float* data, int data_size, float* filter_l, float* filter_r, float* buffer_l, float* buffer_r, int* prev_delays, int* delays);
-
-        //[DllImport("HRTFu_plugin", EntryPoint = "DSP_delay", CallingConvention = System.Runtime.InteropServices.CallingConvention.Cdecl)]
-        //public static extern void DSP_delay([Out] float[] data,[In] int data_size,[In] bool first, [In] float[] filter_l, [In] float[] filter_r, [Out] float[] buffer_signal, [Out] float[] buffer_l, [Out] float[] buffer_r, [Out] int[] delays);
-        //float* data,int data_size,bool first,float* filter_l,float* filter_r,float* buffer_signal,float* buffer_l,float* buffer_r,int* delays
     }
 }
--- HRTFu/HRTFu.cs
+++ HRTFu/HRTFu.cs
@@ -1,61 +1,62 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Audio;
 using Eigen_HRTF_plugin;
-using System.Text;
-using System.IO;
-using UnityEngine.Scripting;
-using Unity.Audio;
-using Unity.Jobs;
 using UnityEngine.Profiling;
+using FaustUtilities_BPF;
 
 public class HRTFu : MonoBehaviour
 {
     private float[] filter_l;
     private float[] filter_r;
+    private float[] filter_l2;
+    private float[] filter_r2;
     private float[] buffer_l;
     private float[] buffer_r;
-    private float[] extra_buffer;
-    private float[] buffer_signal;
-    private bool first = true;
+    private float[] signal_buffer;
     private int[] delays;
     private int[] prev_delays;
     private int[] idxs;
-    private float distance = 20;
+    private int[] prev_idxs;
+    private float distance = 0;
     private float elevation = 0;
     private float azimuth = 0;
+    [Tooltip("Define a 'centimeter' in the virtual envirioment.")]
     public float scale = 0.01f;
     public enum Pinnas : int { Small = 0, Large = 1 };
     public Pinnas pinna;
     public GameObject listener = null;
+    [Tooltip("Gain in dB. Modification of the gain may change the subjective perception of distance. We strongly recommend do not modify this parameter. Use it at your own discretion. Default value 0 dB")]
+    public float gain = 0f;    
     private AudioSource audio_source;
-    private bool _isPlaying = false;    
-    public List<float> samples;    
+    private bool _isPlaying = false;
+    private bool _isVirtual = false;    
+    //Listener object    
+    // Start is called before the first frame update  
+
 
     void Awake()
     {
         filter_l = new float[257*2];
-        filter_r = new float[257*2];        
-        buffer_l = new float[1024];
-        buffer_r = new float[1024];
-        extra_buffer = new float[128];
-        buffer_signal = new float[256];
+        filter_r = new float[257*2];
+        filter_l2 = new float[257 * 2];
+        filter_r2 = new float[257 * 2];        
+        buffer_l = new float[589];
+        buffer_r = new float[589];
+        signal_buffer = new float[128];
         delays = new int[2];
         prev_delays = new int[2];
         idxs = new int[2];
+        prev_idxs = new int[2];
         idxs[0] = -2;
         idxs[1] = -2;
+        prev_idxs[0] = -1;
+        prev_idxs[1] = -1;
         delays[0] = 0;
         delays[1] = 0;
         prev_delays[0] = 0;
-        prev_delays[1] = 0;
-
-        sampler = CustomSampler.Create("Extra");
-        sampler2 = CustomSampler.Create("Audio_processing");
-
-        AudioConfiguration config = AudioSettings.GetConfiguration();
-        CheckAudioSource();
+        prev_delays[1] = 0;        
+       
+        CheckAudioSource();        
         if (listener == null)
         {
             //Seek audio listeners in scene
@@ -71,20 +72,22 @@ public class HRTFu : MonoBehaviour
                 listener = listeners[0].gameObject;
             }
         }
-        Eigen_HRTF.eigen_init();        
+        Eigen_HRTF.eigen_init(Application.streamingAssetsPath+"/HRTFu/");        
         _isPlaying = audio_source.isPlaying;        
-        samples = new List<float>();
     }
 
     // Update is called once per frame
     void LateUpdate()
-    {        
+    {
+        _isPlaying = audio_source.isPlaying;
+        _isVirtual = audio_source.isVirtual;        
+        if (!_isPlaying) return;        
         float tmp_d = distance;
         float tmp_e = elevation;
         float tmp_a = azimuth;
-        //Calculate distance between listener and sound source	
+        //Calculate distance between listener and sound source  
         distance = Mathf.Abs(Vector3.Distance(transform.position, listener.transform.position)) / scale;
-        //Calculate diretion vector between listener and sound source	
+        //Calculate diretion vector between listener and sound source   
         Vector3 dir = (transform.position - listener.transform.position).normalized;        
         //Calculate angle of elevation between listener and sound source        
         if (Vector3.Cross(listener.transform.right, Vector3.ProjectOnPlane(dir, listener.transform.up)) == Vector3.zero)
@@ -96,7 +99,7 @@ public class HRTFu : MonoBehaviour
         {
             Vector3 dirE = Vector3.ProjectOnPlane(dir, listener.transform.right);
             elevation = -Vector3.SignedAngle(listener.transform.forward, dirE, listener.transform.right);
-        }
+        }       
         elevation = elevation % 180 == 0 ? 0 : elevation;        
         if (elevation < -90f)
         {
@@ -112,11 +115,16 @@ public class HRTFu : MonoBehaviour
         if (azimuth < 0f)
         {
             azimuth = 360f + azimuth;
-        }        
-        _isPlaying = audio_source.isPlaying;
-        if (tmp_d == distance && tmp_e == elevation && tmp_a == azimuth) { sampler.End(); return; }        
-        Eigen_HRTF.get_filters(distance, elevation, azimuth, (int)pinna, filter_l, filter_r, delays, idxs);
+        }                
+        if (tmp_d == distance && tmp_e == elevation && tmp_a == azimuth) {            
+        return; }
+        Eigen_HRTF.get_filters(distance, elevation, azimuth, (int)pinna, filter_l, filter_r, delays, idxs);                
 
+    }
+    public void reset_buffer()
+    {
+        buffer_l=new float[589];
+        buffer_r = new float[589];
     }
 
     private void CheckAudioSource()
@@ -126,12 +134,9 @@ public class HRTFu : MonoBehaviour
         audio_source = this.GetComponent<AudioSource>();
     }
     void OnAudioFilterRead(float[] data, int channels)
-    {        
-        unsafe
-        {
-            if (_isPlaying)                
-                Eigen_HRTF.DSP(data, data.Length, filter_l, filter_r, buffer_l, buffer_r, prev_delays, delays);
-        }                
-    }    
+    {
+        if (!_isPlaying) return;                
+        Eigen_HRTF.DSP(data, data.Length, filter_l, filter_r, filter_l2, filter_r2, buffer_l, buffer_r, prev_delays, delays, prev_idxs, idxs, gain);           
+    }  
 
 }
